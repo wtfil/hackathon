c1e7e1c71bb90cc849227cbce2b7b6e719e7f9dd
wice24 <wice24@gmail.com>
2012-04-21 21:22:45 -0400

    add test example

--- application.js
+++ application.js
@@ -1,1 +1,29 @@
-alert
+var APP = function(){
+	var app = function(){
+		var self = this;
+		self.init();
+	}
+	app.prototype = {
+		init: function(){
+			FB.init({
+				appId      : '403886702969818',
+				channelUrl : 'http://friday.incubus.univ.kiev.ua/index.html', // Channel File
+				status     : true, // check login status
+				cookie     : true, // enable cookies to allow the server to access the session
+				xfbml      : true  // parse XFBML
+			});
+			FB.login(function(response) {
+				if (response.authResponse) {
+					console.log('Welcome!  Fetching your information.... ');
+					console.dir(response);
+					FB.api('/me', function(response) {
+						console.log('Good to see you, ' + response.name + '.');
+					});
+				} else {
+					console.log('User cancelled login or did not fully authorize.');
+				}
+			});	
+		}
+	}
+	return new app();
+}
