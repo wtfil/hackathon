38825c9d4029d82ade09b1320385b80b1ae50c6c
Sabov <alexsabov@gmail.com>
2012-04-22 05:58:23 +0300

    add sort by time

--- events.js
+++ events.js
@@ -14,6 +14,26 @@ var UsersEvents = (function(){
 				self.getFullEvents(response, callback);
 			});
 		},
+		getEventsByTime: function(events, hours){
+			var self = this;
+			var res = [];
+			var now = Math.round(new Date().getTime()/1000);
+			events.forEach(function(value){
+				var start = value.start_time;
+				var end   = value.end_time;
+				var eventLength = self.timeDiff(start, end);
+				var timeToBegin = self.timeDiff(now, start);
+				var timeToEnd   = self.timeDiff(now, end);
+				if((timeToBegin <= 0 && timeToEnd < hours) || timeToBegin > hours) return;
+				else res.push(value);
+			});
+			return res;
+		},
+		timeDiff: function(start, end){
+			var self = this;
+			var diff = end - start;
+			return Math.round(diff/3600);
+		},
 		getFullEvents: function(data, callback){
 			var self = this;
 			var res = {};
@@ -25,12 +45,13 @@ var UsersEvents = (function(){
 				res[eid] || (res[eid] = []);
 				res[eid].push(event.uid);
 			});
+			var len = Object.keys(res).length;
 			Object.keys(res).forEach(function(key){
 				self.getEventDetail(key, function(obj){
 					var data = {users: res[key]};
 					jQuery.extend(data, obj[0]);
 					events.push(data);
-					if((Object.keys(res).length == ++count) && callback) callback(events);
+					if((len == ++count) && callback) callback(events);
 				});
 			});
 		},
