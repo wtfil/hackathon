1a9912d1c34d999293b11cb4d407abceafd5817b
wice24 <wice24@gmail.com>
2012-04-21 23:17:21 -0400

    complette auth

--- application.js
+++ application.js
@@ -1,28 +1,43 @@
 var APP = function(){
 	var app = function(){
 		var self = this;
-		self.init();
+		self.accessToken;
+		self.uid;
+		self.init(function(){
+			console.log(self.accessToken);
+		});
 	}
 	app.prototype = {
-		init: function(){
+		conf:{
+			appId      : '403886702969818',
+			channelUrl : 'http://friday.incubus.univ.kiev.ua/index.html',
+		},
+		init: function(callback){
+			var self = this;
 			FB.init({
-				appId      : '403886702969818',
-				channelUrl : 'http://friday.incubus.univ.kiev.ua/index.html', // Channel File
+				appId      : self.conf.appId,
+				channelUrl : self.conf.channelUrl,
 				status     : true, // check login status
 				cookie     : true, // enable cookies to allow the server to access the session
 				xfbml      : true  // parse XFBML
 			});
-			FB.login(function(response) {
-				if (response.authResponse) {
-					console.log('Welcome!  Fetching your information.... ');
-					console.dir(response);
-					FB.api('/me', function(response) {
-						console.log('Good to see you, ' + response.name + '.');
-					});
+			FB.getLoginStatus(function(response) {
+				if (response.status === 'connected') {
+					self.uid = response.authResponse.userID;
+					self.accessToken = response.authResponse.accessToken;
+					callback(response.authResponse.accessToken);
+				} else if (response.status === 'not_authorized') {
+					alert( 'the user is logged in to Facebook, but has not authenticated your app')
 				} else {
-					console.log('User cancelled login or did not fully authorize.');
+					FB.login(function(response) {
+						if (response.authResponse) {
+							callback(response.authResponse.accessToken);
+						} else {
+							alert( 'User cancelled login or did not fully authorize.');
+						}
+					});
 				}
-			});	
+			 });
 		}
 	}
 	return new app();
